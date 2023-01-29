bc4923e5d8c31c1a5cec529f0deaf6469cf521db
Tharles Ribeiro Pereira <83468146+tharlinho2@users.noreply.github.com>
2023-01-29 13:56:35 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   
-  config.hosts.clear
+  config.hosts << "reminde.up.railway.app"
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
