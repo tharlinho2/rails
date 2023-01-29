de0ac70091e275758a3ad9050c19e2054be6f78b
Tharles Ribeiro Pereira <83468146+tharlinho2@users.noreply.github.com>
2023-01-29 13:56:52 -0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  
+  config.hosts << "reminde.up.railway.app"
+
 
   # Show full error reports.
   config.consider_all_requests_local = true
